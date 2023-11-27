b11384be374a5cc8b289781fbd0cf9238c288ed7
Japhet1 <40357151+Japhet1@users.noreply.github.com>
2023-11-27 02:05:30 +0000

    update

--- src/components/NavBarStick.jsx
+++ src/components/NavBarStick.jsx
@@ -26,7 +26,7 @@ const NavBarStick = () => {
         </div>
         <div className="bg-about text-center bg-center text-black">
           <div className=" bg-gray-700 px-20 py-40 md:p-40 lg:p-48 mb-auto bg-opacity-50">
-            <h1 className="text-5xl md:text-7xl font-gruppo text-white shadow-lg font-bold">Qui somme nous</h1>
+            <h1 className="text-5xl md:text-7xl font-gruppo text-white font-bold">Qui somme nous</h1>
           </div>
         </div>
     </main>
